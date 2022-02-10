babcf45932e45a3a532627fede83332e23ee1918
rafalghul <64754323+rafalghul@users.noreply.github.com>
2022-02-09 23:28:46 -0500

    Updated api_sample_request

--- api_sample_request.py
+++ api_sample_request.py
@@ -14,26 +14,25 @@ import json
 
 # Test prediction
 # Copied from api_test.py
-prediction_below_50k = {
-            "individual_id": {
-                "age": 39,
-                "workclass": "State-gov",
-                "fnlgt": 77516,
-                "education": "Bachelors",
-                "education_num": 13,
-                "marital_status": "Never-married",
-                "occupation": "Adm-clerical",
-                "relationship": "Not-in-family",
-                "race": "White",
-                "sex": "Male",
-                "capital_gain": 2174,
-                "capital_loss": 0,
-                "hours_per_week": 40,
-                "native_country": "United-States"
-            }
-        }
+prediction_below_50K= {
+        "age": 50,
+        "workclass": "State-gov",
+        "fnlgt": 83311,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 0,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States",
+        "salary": "<=50K"
+    }
 
-example = json.dumps(prediction_below_50k)
+example = json.dumps(prediction_below_50K)
 
 # Load app URL
 heroku_url = 'https://ml-census.herokuapp.com/inference/'
